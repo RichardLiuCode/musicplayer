d63f18281a0e82e1639760f3947a1efab858487c
Richard Liu <richardadesigner@gmail.com>
2026-04-18 18:09:58 -0700

    update

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const player = document.getElementById("player");
+const playerTimeline = document.getElementById("player-bar");
 let isPlaying = false;
+let isTimelineBarDraging = false;
+let subtitlePosition = 0;
+setInterval(function () {
+    document.getElementById("subtitleText").style.transform = `translateX(${subtitlePosition}px)`;
+    subtitlePosition = subtitlePosition + 1;
+    if (subtitlePosition > 200) {
+        subtitlePosition = -300;
+    }
+}, 40);
 document.getElementById("uploadBtn").addEventListener("click", function () {
     const selectedFile = document.getElementById("file-selector").files[0];
+    const audioName = selectedFile.name;
     if (selectedFile) {
         const reader = new FileReader();
         reader.onload = function (file) {
             const audio = file.target.result;
             document.getElementById("player-source").src = audio;
             player.load();
-            player.addEventListener("loadedmetadata", function () {
-                document.getElementById("player-bar").max = player.duration;
-            });
+            document.querySelector(".homePage").style.display = "none";
+            document.querySelector(".playPage").style.display = "flex";
+            document.getElementById("subtitleText").textContent = audioName;
         };
         reader.readAsDataURL(selectedFile);
     }
@@ -18,16 +29,75 @@ document.getElementById("uploadBtn").addEventListener("click", function () {
 
 document.getElementById("play-button").addEventListener("click", function () {
     if (!isPlaying) {
-        this.textContent = "⏸";
+        this.style.backgroundImage = "url(\"pauseIcon.png\")";
+        this.style.backgroundSize = "50%";
         isPlaying = true;
         player.play();
     } else {
-        this.textContent = "▶";
+        this.style.backgroundImage = "url(\"playIcon.png\")";
+        this.style.backgroundSize = "70%";
         isPlaying = false;
         player.pause();
     }
 });
 
+
+function updateTimelineColor() {
+    document.getElementById("videoProgressBar").max = document.getElementById("videoProgressBar").max;
+    document.getElementById("videoProgressBar").value = playerTimeline.value;
+}
+
+function displayTime(currentTime, duration) {
+    const currentTimeObject = {
+        seconds: currentTime % 60,
+        minutes: Math.floor(currentTime / 60),
+        displayString: function () {
+            return this.minutes.toString().padStart(2, "0") + ":" + Math.round(this.seconds).toString().padStart(2, "0");
+        }
+    };
+    const maxTimeObject = {
+        seconds: duration % 60,
+        minutes: Math.floor(duration / 60),
+        displayString: function () {
+            return this.minutes.toString().padStart(2, "0") + ":" + Math.round(this.seconds).toString().padStart(2, "0");
+        }
+    };
+    document.getElementById("timeDisplay").innerHTML = currentTimeObject.displayString() + "&nbsp/&nbsp" + maxTimeObject.displayString();
+    document.querySelector(".play-area-wrapper").style.display = "flex";
+}
+playerTimeline.addEventListener("mousedown", function () {
+    isTimelineBarDraging = true;
+    updateTimelineColor();
+});
+playerTimeline.addEventListener("mouseup", function () {
+    isTimelineBarDraging = false;
+    player.currentTime = playerTimeline.value;
+    updateTimelineColor();
+});
+playerTimeline.addEventListener("click", function () {
+    updateTimelineColor();
+});
+player.addEventListener("loadedmetadata", function () {
+    playerTimeline.max = player.duration;
+    document.getElementById("videoProgressBar").max = player.duration;
+    displayTime(player.currentTime, playerTimeline.max);
+});
 player.addEventListener("timeupdate", function () {
-    document.getElementById("player-bar").value = player.currentTime;
+    if (!isTimelineBarDraging) {
+        playerTimeline.value = player.currentTime;
+        displayTime(player.currentTime, player.duration);
+    }
+    updateTimelineColor();
+
+});
+player.addEventListener("ended", function () {
+    playerTimeline.value = parseFloat(playerTimeline.max);
+});
+
+document.getElementById("closePlayerBtn").addEventListener("click", function () {
+    if (isPlaying) {
+        player.pause();
+    }
+    document.querySelector(".homePage").style.display = "revert";
+    document.querySelector(".playPage").style.display = "none";
 });
